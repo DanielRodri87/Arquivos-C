f0a5ee898819809ea847cec624ab8fba7d25250d
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-12-25 16:28:04 -0300

    att

--- 1TREINOFERIAS/Teste1/banco.c
+++ 1TREINOFERIAS/Teste1/banco.c
@@ -66,7 +66,7 @@ void criarConta(FILE *arq)
     switch (strcmp(criarSenha, criarSenha2))
     {
     case 0:
-        fprintf(arq, "%s %s\n", criarUser, criarSenha);
+        fprintf(arq, "%s %s %.2f\n", criarUser, criarSenha, 0.0);
         printf("Conta criada com sucesso!\n");
         break;
 
